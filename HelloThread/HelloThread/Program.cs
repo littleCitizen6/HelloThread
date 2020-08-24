a0cc6dcad47db9e1194cd077910f8116dd51507c
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-24 11:20:45 +0300

    added project

---
+++ HelloThread/HelloThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
