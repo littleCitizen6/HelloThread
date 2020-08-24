618633032677928c1da863f1a5965c6d63472446
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-24 21:00:46 +0300

    finish section 1

--- HelloThread/HelloThread/Program.cs
+++ HelloThread/HelloThread/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HelloThread
 {
     class Program
     {
+        public static void PrintMany(string str, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(str);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread mamas = new Thread(() => PrintMany("mamas", 1000));
+            Thread empire = new Thread(() => PrintMany("empire", 1000));
+            mamas.IsBackground = false;
+            empire.IsBackground = false;
+            mamas.Start();
+            empire.Start();
         }
     }
 }
